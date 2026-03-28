7983680a0d53cc7b6dcc2e32dc0c7222d536fba8
NadyanGeeks <pratamanadyan@gmail.com>
2026-03-28 15:35:27 +0700

    membuat navigation

---
+++ components/Header.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+import { cn } from "@/src/lib/utils";
+
+const Header = () => {
+  const pathname = usePathname();
+  return (
+    <header>
+      <div className="main-container inner">
+        <Link href="/">
+          <div className="flex items-center gap-3">
+            <Image
+              src="/assets/favicon.ico"
+              alt="Cryptoring Logo"
+              width={40}
+              height={40}
+            />
+            <p className="text-2xl text-white font-medium">Cryptoring</p>
+          </div>
+        </Link>
+
+        <nav>
+          <Link
+            href="/"
+            className={cn("nav-link", {
+              "is-active": pathname === "/",
+              "is-home": true,
+            })}
+          >
+            Home
+          </Link>
+
+          <p>Search Modal</p>
+
+          <Link
+            href="/coins"
+            className={cn("nav-link", {
+              "is-active": pathname === "/coins",
+            })}
+          >
+            All Coins
+          </Link>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
